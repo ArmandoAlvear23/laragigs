4444d5fc2df63b30ba71af04e27efa3cfb92aa2b
Armando Alvear <armando.alvear01@outlook.com>
2023-09-28 09:02:44 -0500

    add layout and section directives

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,3 +1,7 @@
+@extends('layout')
+
+@section('content')
+
 <h1>{{$heading}}</h1>
 
 @unless(count($listings) == 0)
@@ -16,3 +20,5 @@
 @else
     <p>No listings found</p>
 @endunless
+
+@endsection
