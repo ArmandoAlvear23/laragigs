73a8b1ccbe728b6f14d69d66455ee627a9b5831a
Armando Alvear <armando.alvear01@outlook.com>
2023-09-28 13:28:52 -0500

    add listing controller and refactor routes code

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    // Get and show all listings
+    public function index() {
+        return view('listings.index', [
+            'heading' => 'Latest Listings',
+            'listings' => Listing::all()
+        ]);
+    }
+
+    // Get and show single listing
+    public function show(Listing $listing) {
+        return view('listings.show', [
+            'listing' => $listing
+        ]);
+    }
+}
