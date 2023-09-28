f1262fe3637776a9ee8345a95941a9792dd79bd5
Armando Alvear <armando.alvear01@outlook.com>
2023-09-28 09:44:25 -0500

    add asset method for image url

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -12,8 +12,8 @@
     <div class="flex">
         <img
             class="hidden w-48 mr-6 md:block"
-            src="images/no-image.png"
-            alt=""
+            src="{{asset('images/no-image.png')}}"
+            alt="company logo"
         />
         <div>
             <h3 class="text-2xl">
