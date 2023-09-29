54dfaed01644c141300c18db3ca0d1ccb5c2cc3b
Armando Alvear <armando.alvear01@outlook.com>
2023-09-29 01:58:20 -0500

    modified seed and migration files for testing user and listing relationship

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
@@ -13,8 +14,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(5)->create();
+        // \App\Models\User::factory(5)->create();
 
+        $user = User::factory()->create([
+            'name' => 'John Doe',
+            'email' => 'john@gmail.com'
+        ]);
+
+        Listing::factory(6)->create([
+            'user_id' => $user->id
+        ]);
+
+        /*
         Listing::create([
             'title' => 'Laravel Senior Developer',
             'tags' => 'laravel, javascript',
@@ -34,7 +45,6 @@ class DatabaseSeeder extends Seeder
             'website' => 'https://www.rgvwebbuilders.com',
             'description' => 'We are looking for a motivated Full-Stack Engineer to help us build amazing websites for our awesome customers!'
         ]);
-
-        Listing::factory(6)->create();
+        */
     }
 }
